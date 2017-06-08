4e50fec35c3064debd194b9547f1b298cf4aee8b
Jessica Stokes <hello@jessicastokes.net>
2017-06-07 21:10:10 -0700

    Put language mapping in its own file

--- src/consumeIndex.js
+++ src/consumeIndex.js
@@ -1,42 +1,4 @@
-// Missing from this:
-//  * 'ar'
-//  * 'c'
-//  * 'cj'
-//  * 'cr'
-//  * 'fl'
-//  * 'ga'
-//  * 'h'
-//  * 'ir' for 'Iranian' (there doesn't appear to be _one_ 'Iranian' language)
-//  * 'sc'
-const LANGUAGE_MAP = {
-  a:  'en-AU',
-  af: 'af',
-  ca: 'ca',
-  ch: 'zh',
-  cz: 'cs',
-  d:  'da',
-  du: 'nl',
-  e:  'en',
-  f:  'fr',
-  fi: 'fi',
-  g:  'de',
-  gr: 'el',
-  hu: 'hu',
-  i:  'it',
-  is: 'he',
-  j:  'jp',
-  k:  'kr',
-  m:  'es-MX',
-  n:  'no',
-  nw: 'no',
-  p:  'pt',
-  pl: 'pl',
-  po: 'pl',
-  r:  'ru',
-  s:  'es',
-  sw: 'sv',
-  t:  'tr'
-};
+const LANGUAGE_MAP = require('./languages');
 
 function findTextNodes(nodeList) {
   return Array.from(nodeList)
---
+++ src/languages.js
@@ -0,0 +1,39 @@
+// Missing from this:
+//  * 'ar'
+//  * 'c'
+//  * 'cj'
+//  * 'cr'
+//  * 'fl'
+//  * 'ga'
+//  * 'h'
+//  * 'ir' for 'Iranian' (there doesn't appear to be _one_ 'Iranian' language)
+//  * 'sc'
+module.exports = {
+  a:  'en-AU',
+  af: 'af',
+  ca: 'ca',
+  ch: 'zh',
+  cz: 'cs',
+  d:  'da',
+  du: 'nl',
+  e:  'en',
+  f:  'fr',
+  fi: 'fi',
+  g:  'de',
+  gr: 'el',
+  hu: 'hu',
+  i:  'it',
+  is: 'he',
+  j:  'jp',
+  k:  'kr',
+  m:  'es-MX',
+  n:  'no',
+  nw: 'no',
+  p:  'pt',
+  pl: 'pl',
+  po: 'pl',
+  r:  'ru',
+  s:  'es',
+  sw: 'sv',
+  t:  'tr'
+};
