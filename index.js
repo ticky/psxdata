09a3052c0e8d102d22d2e7dfbc74a21c49aaad10
Jessica Stokes <hello@jessicastokes.net>
2017-06-11 14:50:55 -0700

    Add JS module main entrypoint

---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+  ps1: {
+    'ntsc-j': {
+      index: require('./ps1/ntsc-j.json')
+    },
+    'ntsc-uc': {
+      index: require('./ps1/ntsc-uc.json')
+    },
+    'pal': {
+      index: require('./ps1/pal.json')
+    }
+  },
+  ps2: {
+    'ntsc-j': {
+      index: require('./ps2/ntsc-j.json')
+    },
+    'ntsc-uc': {
+      index: require('./ps2/ntsc-uc.json')
+    },
+    'pal': {
+      index: require('./ps2/pal.json')
+    }
+  },
+  psp: {
+    'ntsc-j': {
+      index: require('./psp/ntsc-j.json')
+    },
+    'ntsc-uc': {
+      index: require('./psp/ntsc-uc.json')
+    },
+    'pal': {
+      index: require('./psp/pal.json')
+    }
+  }
+};
